c90d85bfefd0037ec0ba11481fa62a752976efea
Brandon Carter <brandon.d.carter99@gmail.com>
2021-06-04 16:56:02 -0400

    Added the functions get_withdrawals, get_totals, and truncate. As well as completed the create_spend_chart function

--- budget.py
+++ budget.py
@@ -11,7 +11,8 @@ class Category:
             items += f'{item["description"][0:23]:23}' + f'{item["amount"]:>7.2f}' + '\n'
             total += item['amount']
 
-        output = title +
+        output = title + items + f'Total: {str(total)}'
+        return output
 
     def deposit(self, dep_amount, description=''):
         '''
@@ -65,7 +66,66 @@ class Category:
         else:
             return False
 
+    def get_withdrawals(self):
+        total = 0
+        for item in self.ledger:
+            if item['amount'] < 0:
+                total += item['amount']
+            else:
+                continue
+        return total
+
+
+def truncate(n):
+    multiplier = 10
+    return int(n * multiplier) / multiplier
+
+def get_totals(categories):
+    total = 0
+    breakdown = []
+    for category in categories:
+        total += category.get_withdrawals()
+    rounded = list(map(lambda x: truncate(x/total), breakdown))
+    return rounded
 
 
 def create_spend_chart(categories):
-    pass
+    '''
+    Tales a list of categories as an argument. It should return a string that is a bar chart
+    '''
+    res = 'Percentage spent by category\n'
+    i = 100
+    totals = get_totals(categories)
+    while i >= 0:
+        cat_spaces = ' '
+        for total in totals:
+            if total * 200 >= 1:
+                cat_spaces += 'o '
+            else:
+                cat_spaces += ' '
+        res += str(i).rjust(3) + '|' + cat_spaces + ('\n')
+        i -= 10
+
+    dashes = '-' + '---'*len(categories)
+    names = []
+    x_axis = ''
+    for category in categories:
+        names.append(category.name)
+
+    maxi = max(names, key=len)
+
+    for x in range(len(maxi)):
+        nameStr = '    '
+        for name in names:
+            if x >= len(name):
+                nameStr += '  '
+            else:
+                nameStr += name[x] +' '
+
+        if(x != len(maxi) -1):
+            nameStr += '\n'
+
+        x_axis += nameStr
+
+    res += dashes.rjust(len(dashes) + 4) + '\n' + x_axis
+    return res
